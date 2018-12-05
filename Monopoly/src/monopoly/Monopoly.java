c4bfaa4e2c9a7409d70772dbf54fb409f70ca5fc
IceHawk246 <45637919+IceHawk246@users.noreply.github.com>
2018-12-05 13:59:33 -0500

    Added hello world line

--- Monopoly/src/monopoly/Monopoly.java
+++ Monopoly/src/monopoly/Monopoly.java
@@ -16,6 +16,8 @@ public class Monopoly {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("hello world!");
     }
     
 }
