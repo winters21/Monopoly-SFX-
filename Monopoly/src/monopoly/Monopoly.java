4f69fadd7c0934fb5477d04051c075e489384ea2
Derpy-Kirby <45669088+Derpy-Kirby@users.noreply.github.com>
2018-12-06 20:19:14 -0500

    iufhif

--- Monopoly/src/monopoly/Monopoly.java
+++ Monopoly/src/monopoly/Monopoly.java
@@ -1,15 +1,12 @@
 /*
 MONOPOLY ASSIGNMENT
 By: Shawn, Chris, Gabe, and Bryce
-
+ICS3U
 */
 package monopoly;
 
 public class Monopoly {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         //This is where Code will go
 
