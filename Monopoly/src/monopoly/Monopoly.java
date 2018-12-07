f047ed3a1dc965ba68ea2733b6dd6df540fe2f58
Derpy-Kirby <45669088+Derpy-Kirby@users.noreply.github.com>
2018-12-06 20:34:40 -0500

    Update Monopoly.java

--- Monopoly/src/monopoly/Monopoly.java
+++ Monopoly/src/monopoly/Monopoly.java
@@ -1,18 +1,20 @@
 /*
 MONOPOLY ASSIGNMENT
-By: Shawn, Chris, Gabe, and Bryce
+By: Shawn Sun, Christopher Jordan, Gabriel Martell, and Bryce Batten
 ICS3U
 */
 package monopoly;
 
+import javax.swing.JFrame;
+
 public class Monopoly {
     
     public static void main(String[] args) {
-        //This is where Code will go
-        
-        //Trolollolol
-        
-        //g8yfg38ygfr873gh87q3h873h783rh8rhg8rhg8ug
-    }
+        JFrame HUD = new JFrame();
+        HUD.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        HUD.setSize(850, 1000);
+        HUD.setLayout(null);
+
+           }
     
 }
