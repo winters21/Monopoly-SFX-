7a792f847c76f9ce545475252ecfe76a77627693
Derpy-Kirby <45669088+Derpy-Kirby@users.noreply.github.com>
2018-12-06 21:14:20 -0500

    added frame

--- Monopoly/src/BoardScreen.java
+++ Monopoly/src/BoardScreen.java
@@ -32,11 +32,11 @@ public class BoardScreen extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 1071, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 852, Short.MAX_VALUE)
         );
 
         pack();
